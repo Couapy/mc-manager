113012e5edad38def19e539a423b685cca16927c
Maël <39948565+Couapy@users.noreply.github.com>
2021-04-16 07:50:14 +0000

    refactor(urls): including path in a server path

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 
 from core import views
 
@@ -7,11 +7,13 @@ urlpatterns = [
     path('', views.DefaultView.as_view(), name='index'),
     path('public-servers/', views.ServerListView.as_view(), name="public-servers"),
     path('manage/', views.ServerManageView.as_view(), name='manage'),
-    path('server/add/', views.ServerCreateView.as_view(), name="add"),
-    path('server/<int:id>/edit/', views.ServerEditView.as_view(), name="edit"),
-    path('server/<int:id>/properties/', views.ServerPropertiesView.as_view(), name="properties"),
-    path('server/<int:id>/permissions/', views.ServerPermissionView.as_view(), name="permissions"),
-    path('server/<int:id>/delete/', views.ServerDeleteView.as_view(), name="delete"),
-    path('server/<int:id>/start/', views.ServerStartView.as_view(), name="start"),
-    path('server/<int:id>/stop/', views.ServerStopView.as_view(), name="stop"),
+    path('server/', include([
+        path('add/', views.ServerCreateView.as_view(), name="add"),
+        path('<int:id>/edit/', views.ServerEditView.as_view(), name="edit"),
+        path('<int:id>/properties/', views.ServerPropertiesView.as_view(), name="properties"),
+        path('<int:id>/permissions/', views.ServerPermissionView.as_view(), name="permissions"),
+        path('<int:id>/delete/', views.ServerDeleteView.as_view(), name="delete"),
+        path('<int:id>/start/', views.ServerStartView.as_view(), name="start"),
+        path('<int:id>/stop/', views.ServerStopView.as_view(), name="stop"),
+    ])),
 ]
