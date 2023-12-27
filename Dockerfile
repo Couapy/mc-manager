2ce36b026fa16f929f1c76976e3ace805097beb4
Maël Marchand <m.marchand@ngc-data.fr>
2023-12-27 13:46:45 +0100

    feat: use poetry and update compose.yaml production

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,27 @@
-FROM python:3.9.4-buster
+FROM python:3.11.5-alpine3.18
+
+ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWRITEBYTECODE 1
+
+RUN apk update && apk add --no-cache tzdata
+ENV TZ=Europe/Paris
 
 WORKDIR /app
 
-# Install dependencies
-COPY requirements.txt .
-RUN pip -q install -r requirements.txt
-RUN apt-get -qq update && apt-get -yqq install default-jre
+COPY pyproject.toml poetry.lock ./
+RUN pip install poetry==1.6.1 \
+  && poetry config virtualenvs.create false \
+  && apk add --no-cache libpq-dev \
+  && apk add --no-cache --virtual .build-deps \
+    build-base  \
+  && poetry install --no-interaction --no-ansi \
+  && apk add --no-cache \
+    openjdk17 \
+  && apk del .build-deps
+
+COPY . ./
+
+RUN python manage.py collectstatic --noinput
+
+EXPOSE 80
+CMD ["daphne", "project.asgi:application", "-b", "0.0.0.0", "-p", "80"]
--- project/settings/base.py
+++ project/settings/base.py
@@ -5,32 +5,31 @@ import re
 import mcdwld
 from django.contrib.messages import constants as messages
 
-
 # Constants
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
 # Configuration
 config = configparser.ConfigParser()
-config.read(os.path.join(BASE_DIR, 'config.cfg'))
+config.read(os.path.join(BASE_DIR, "config.cfg"))
 
 
 # Security
-SECRET_KEY = config.get('DJANGO', 'SECRET_KEY')
+SECRET_KEY = config.get("DJANGO", "SECRET_KEY")
 DEBUG = True
-ALLOWED_HOSTS = re.split(', ?', config.get('DJANGO', 'DOMAINS'))
+ALLOWED_HOSTS = ["minecraft.marchand.cloud", "localhost"]
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -38,130 +37,131 @@ AUTH_PASSWORD_VALIDATORS = [
 # Applications
 INSTALLED_APPS = [
     # Django applications
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # Dependencies
-    'social_django',
-    'crispy_forms',
+    "social_django",
+    "crispy_forms",
+    "crispy_bootstrap4",
     # My applications
-    'account',
-    'core',
+    "account",
+    "core",
 ]
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = "project.urls"
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            (os.path.join(BASE_DIR, 'project/templates')),
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            (os.path.join(BASE_DIR, "project/templates")),
         ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'account.context_processors.providers_settings',
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "account.context_processors.providers_settings",
             ],
         },
     },
 ]
-WSGI_APPLICATION = 'project.wsgi.application'
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+WSGI_APPLICATION = "project.wsgi.application"
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 
 # Authentication
 ACCOUNTS_PROVIDERS = [
     {
-        'provider': 'google-oauth2',
-        'name': 'Google',
-        'link': None,
-        'username': None,
+        "provider": "google-oauth2",
+        "name": "Google",
+        "link": None,
+        "username": None,
     },
     {
-        'provider': 'github',
-        'name': 'Github',
-        'link': 'https://github.com/{{ data.login }}',
-        'username': '{{ data.login }}',
+        "provider": "github",
+        "name": "Github",
+        "link": "https://github.com/{{ data.login }}",
+        "username": "{{ data.login }}",
     },
     {
-        'provider': 'twitter',
-        'name': 'Twitter',
-        'link': 'https://twitter.com/{{ data.access_token.screen_name }}/',
-        'username': '@{{ data.access_token.screen_name }}',
+        "provider": "twitter",
+        "name": "Twitter",
+        "link": "https://twitter.com/{{ data.access_token.screen_name }}/",
+        "username": "@{{ data.access_token.screen_name }}",
     },
     {
-        'provider': 'facebook',
-        'name': 'Facebook',
-        'link': None,
-        'username': None,
+        "provider": "facebook",
+        "name": "Facebook",
+        "link": None,
+        "username": None,
     },
 ]
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'social_core.backends.google.GoogleOAuth2',
-    'social_core.backends.github.GithubOAuth2',
-    'social_core.backends.twitter.TwitterOAuth',
-    'social_core.backends.facebook.FacebookOAuth2',
+    "django.contrib.auth.backends.ModelBackend",
+    "social_core.backends.google.GoogleOAuth2",
+    "social_core.backends.github.GithubOAuth2",
+    "social_core.backends.twitter.TwitterOAuth",
+    "social_core.backends.facebook.FacebookOAuth2",
 )
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config.get('GOOGLE', 'KEY')
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config.get('GOOGLE', 'SECRET')
-SOCIAL_AUTH_GITHUB_KEY = config.get('GITHUB', 'KEY')
-SOCIAL_AUTH_GITHUB_SECRET = config.get('GITHUB', 'SECRET')
-SOCIAL_AUTH_TWITTER_KEY = config.get('TWITTER', 'KEY')
-SOCIAL_AUTH_TWITTER_SECRET = config.get('TWITTER', 'SECRET')
-SOCIAL_AUTH_FACEBOOK_KEY = config.get('FACEBOOK', 'KEY')
-SOCIAL_AUTH_FACEBOOK_SECRET = config.get('FACEBOOK', 'SECRET')
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config.get("GOOGLE", "KEY")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config.get("GOOGLE", "SECRET")
+SOCIAL_AUTH_GITHUB_KEY = config.get("GITHUB", "KEY")
+SOCIAL_AUTH_GITHUB_SECRET = config.get("GITHUB", "SECRET")
+SOCIAL_AUTH_TWITTER_KEY = config.get("TWITTER", "KEY")
+SOCIAL_AUTH_TWITTER_SECRET = config.get("TWITTER", "SECRET")
+SOCIAL_AUTH_FACEBOOK_KEY = config.get("FACEBOOK", "KEY")
+SOCIAL_AUTH_FACEBOOK_SECRET = config.get("FACEBOOK", "SECRET")
 
-LOGIN_URL = '/account/login/'
-LOGIN_REDIRECT_URL = '/account/profile/'
-LOGOUT_REDIRECT_URL = '/'
-SOCIAL_AUTH_URL_NAMESPACE = 'social'
-AUTH_PROFILE_MODULE = 'account.Profile'
+LOGIN_URL = "/account/login/"
+LOGIN_REDIRECT_URL = "/account/profile/"
+LOGOUT_REDIRECT_URL = "/"
+SOCIAL_AUTH_URL_NAMESPACE = "social"
+AUTH_PROFILE_MODULE = "account.Profile"
 
 SOCIAL_AUTH_PIPELINE = (
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.user.get_username',
-    'social_core.pipeline.social_auth.associate_by_email',
-    'social_core.pipeline.user.create_user',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.social_auth.associate_by_email",
+    "social_core.pipeline.user.create_user",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
 )
 
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = LOGIN_REDIRECT_URL
 SOCIAL_AUTH_NEW_USER_REDIRECT_URL = LOGIN_REDIRECT_URL
-SOCIAL_AUTH_DISCONNECT_REDIRECT_URL = '/'
+SOCIAL_AUTH_DISCONNECT_REDIRECT_URL = "/"
 
 
 # Files
-VAR_DIR = os.path.join(BASE_DIR, 'var')
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(VAR_DIR, 'static/')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(VAR_DIR, 'media/')
+VAR_DIR = "/data"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(VAR_DIR, "media/")
 
 
 # Locale
-LANGUAGE_CODE = 'fr-fr'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = "fr-fr"
+TIME_ZONE = "UTC"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
@@ -169,23 +169,23 @@ USE_TZ = True
 
 # Messages
 MESSAGE_TAGS = {
-    messages.DEBUG: 'alert-info',
-    messages.INFO: 'alert-info',
-    messages.SUCCESS: 'alert-success',
-    messages.WARNING: 'alert-warning',
-    messages.ERROR: 'alert-danger',
+    messages.DEBUG: "alert-info",
+    messages.INFO: "alert-info",
+    messages.SUCCESS: "alert-success",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger",
 }
 
 
 # Crispy forms
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 
 # Minecraft servers
-MINECRAFT_DOWNLOAD_ROOT = os.path.join(VAR_DIR, 'servers_files')
-MINECRAFT_DATA_ROOT = os.path.join(VAR_DIR, 'servers_data', 'server-%02d')
+MINECRAFT_DOWNLOAD_ROOT = os.path.join(VAR_DIR, "servers_files")
+MINECRAFT_DATA_ROOT = os.path.join(VAR_DIR, "servers_data", "server-%02d")
 MINECRAFT_SERVERS_TYPES = re.split(
-    ', ?', config.get('MINECRAFT', 'SERVER_TYPES'))
+    ", ?", config.get("MINECRAFT", "SERVER_TYPES"))
 MINECRAFT_SERVERS_LOCAL = mcdwld.get_local_versions(directory=MINECRAFT_DOWNLOAD_ROOT)
-MINECRAFT_MCMINMEM = config.get('MINECRAFT', 'MCMINMEM')
-MINECRAFT_MCMAXMEM = config.get('MINECRAFT', 'MCMAXMEM')
+MINECRAFT_MCMINMEM = config.get("MINECRAFT", "MCMINMEM")
+MINECRAFT_MCMAXMEM = config.get("MINECRAFT", "MCMAXMEM")
